42ac89bc8ca06d118ebad180f9bd7fe1b673a101
margara <margara@192.168.0.102>
2013-03-18 23:06:54 +0100

    Bug fix

--- src/nl/vu/cs/querypie/reasoner/actions/PrecompGenericMap.java
+++ src/nl/vu/cs/querypie/reasoner/actions/PrecompGenericMap.java
@@ -16,107 +16,87 @@ import nl.vu.cs.querypie.reasoner.rules.Rule;
 
 public class PrecompGenericMap extends Action {
 
-	private int[][] key_positions;
-	private int[][] positions_to_check;
-	private int[][] pos_constants_to_check;
-	private long[][] value_constants_to_check;
-	private Collection<Long>[] acceptableValues;
-	private Rule[] rules;
+  private int[][] key_positions;
+  private int[][] positions_to_check;
+  private int[][] pos_constants_to_check;
+  private long[][] value_constants_to_check;
+  private Collection<Long>[] acceptableValues;
+  private Rule[] rules;
 
-	private final TByteArray oneKey = new TByteArray(new byte[8]);
-	private final TByteArray twoKeys = new TByteArray(new byte[16]);
-	private final TByte ruleID = new TByte();
-	private final Tuple outputTuple = TupleFactory.newTuple();
+  private final TByteArray oneKey = new TByteArray(new byte[8]);
+  private final TByteArray twoKeys = new TByteArray(new byte[16]);
+  private final TByte ruleID = new TByte();
+  private final Tuple outputTuple = TupleFactory.newTuple();
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public void startProcess(ActionContext context) throws Exception {
+  @SuppressWarnings("unchecked")
+  @Override
+  public void startProcess(ActionContext context) throws Exception {
+    rules = ReasoningContext.getInstance().getRuleset().getAllRulesWithSchemaAndGeneric();
+    key_positions = new int[rules.length][];
+    positions_to_check = new int[rules.length][];
+    acceptableValues = new Collection[rules.length];
+    pos_constants_to_check = new int[rules.length][];
+    value_constants_to_check = new long[rules.length][];
 
-		rules = ReasoningContext.getInstance().getRuleset()
-				.getAllRulesWithSchemaAndGeneric();
-		key_positions = new int[rules.length][];
-		positions_to_check = new int[rules.length][];
-		acceptableValues = new Collection[rules.length];
-		pos_constants_to_check = new int[rules.length][];
-		value_constants_to_check = new long[rules.length][];
+    for (int m = 0; m < rules.length; ++m) {
+      Rule rule = rules[m];
 
-		for (int m = 0; m < rules.length; ++m) {
-			Rule rule = rules[m];
+      // Get the positions of the generic patterns that are used in the
+      // head
+      int[][] shared_vars = rule.getSharedVariablesGen_Head();
+      key_positions[m] = new int[shared_vars.length];
+      for (int i = 0; i < key_positions[m].length; ++i) {
+        key_positions[m][i] = shared_vars[i][0];
+      }
 
-			// Get the positions of the generic patterns that are used in the
-			// head
-			int[][] shared_vars = rule.getSharedVariablesGen_Head();
-			key_positions[m] = new int[shared_vars.length];
-			for (int i = 0; i < key_positions[m].length; ++i) {
-				key_positions[m][i] = shared_vars[i][0];
-			}
+      // Get the positions in the generic variables that should be checked
+      // against the schema
+      shared_vars = rule.getSharedVariablesGen_Precomp();
+      positions_to_check[m] = new int[shared_vars.length];
+      for (int i = 0; i < positions_to_check[m].length; ++i) {
+        positions_to_check[m][i] = shared_vars[i][0];
+      }
 
-			// Get the positions in the generic variables that should be checked
-			// against the schema
-			shared_vars = rule.getSharedVariablesGen_Precomp();
-			positions_to_check[m] = new int[shared_vars.length];
-			for (int i = 0; i < positions_to_check[m].length; ++i) {
-				positions_to_check[m][i] = shared_vars[i][0];
-			}
+      // Get the elements from the precomputed tuples that should be
+      // checked
+      if (shared_vars.length > 1) {
+        throw new Exception("Not implemented yet");
+      }
 
-			// Get the elements from the precomputed tuples that should be
-			// checked
-			if (shared_vars.length > 1) {
-				throw new Exception("Not implemented yet");
-			}
+      acceptableValues[m] = rule.getPrecomputedTuples().getSortedSet(shared_vars[0][1]);
+      pos_constants_to_check[m] = rule.getPositionsConstantGenericPattern();
+      value_constants_to_check[m] = rule.getValueConstantGenericPattern();
+    }
+  }
 
-			acceptableValues[m] = rule.getPrecomputedTuples().getSortedSet(
-					shared_vars[0][1]);
+  @Override
+  public void process(Tuple tuple, ActionContext context, ActionOutput actionOutput) throws Exception {
+    for (int m = 0; m < rules.length; m++) {
+      // Does the input match with the generic pattern?
+      if (!nl.vu.cs.querypie.reasoner.support.Utils.tupleMatchConstants(tuple, pos_constants_to_check[m], value_constants_to_check[m])) {
+        continue;
+      }
+      TLong t = (TLong) tuple.get(positions_to_check[m][0]);
+      if (acceptableValues[m].contains(t.getValue())) {
+        ruleID.setValue(m);
+        if (key_positions[m].length == 1) {
+          Utils.encodeLong(oneKey.getArray(), 0, ((TLong) tuple.get(key_positions[m][0])).getValue());
+          outputTuple.set(oneKey, ruleID, tuple.get(positions_to_check[m][0]));
+        } else { // Two keys
+          Utils.encodeLong(twoKeys.getArray(), 0, ((TLong) tuple.get(key_positions[m][0])).getValue());
+          Utils.encodeLong(twoKeys.getArray(), 8, ((TLong) tuple.get(key_positions[m][1])).getValue());
+          outputTuple.set(twoKeys, ruleID, tuple.get(positions_to_check[m][0]));
+        }
+        actionOutput.output(outputTuple);
+      }
+    }
+  }
 
-			pos_constants_to_check[m] = rule
-					.getPositionsConstantGenericPattern();
-			value_constants_to_check[m] = rule.getValueConstantGenericPattern();
-		}
-	}
-
-	@Override
-	public void process(Tuple tuple, ActionContext context,
-			ActionOutput actionOutput) throws Exception {
-
-		for (int m = 0; m < rules.length; m++) {
-
-			// Does the input match with the generic pattern?
-			if (!nl.vu.cs.querypie.reasoner.support.Utils.tupleMatchConstants(
-					tuple, pos_constants_to_check[m],
-					value_constants_to_check[m])) {
-				continue;
-			}
-
-			TLong t = (TLong) tuple.get(positions_to_check[m][0]);
-
-			if (acceptableValues[m].contains(t.getValue())) {
-				ruleID.setValue(m);
-				if (key_positions[m].length == 1) {
-					Utils.encodeLong(oneKey.getArray(), 0,
-							((TLong) tuple.get(key_positions[m][0])).getValue());
-					outputTuple.set(oneKey, ruleID,
-							tuple.get(positions_to_check[m][0]));
-
-				} else { // Two keys
-					Utils.encodeLong(twoKeys.getArray(), 0,
-							((TLong) tuple.get(key_positions[m][0])).getValue());
-					Utils.encodeLong(twoKeys.getArray(), 8,
-							((TLong) tuple.get(key_positions[m][1])).getValue());
-					outputTuple.set(twoKeys, ruleID,
-							tuple.get(positions_to_check[m][0]));
-				}
-				actionOutput.output(outputTuple);
-			}
-		}
-
-	}
-
-	@Override
-	public void stopProcess(ActionContext context, ActionOutput actionOutput)
-			throws Exception {
-		rules = null;
-		acceptableValues = null;
-		key_positions = null;
-		positions_to_check = null;
-	}
+  @Override
+  public void stopProcess(ActionContext context, ActionOutput actionOutput) throws Exception {
+    rules = null;
+    acceptableValues = null;
+    key_positions = null;
+    positions_to_check = null;
+  }
 }
--- src/nl/vu/cs/querypie/reasoner/actions/PrecompGenericReduce.java
+++ src/nl/vu/cs/querypie/reasoner/actions/PrecompGenericReduce.java
@@ -22,118 +22,108 @@ import nl.vu.cs.querypie.reasoner.support.sets.Tuples;
 
 public class PrecompGenericReduce extends Action {
 
-	private int[][][] pos_head_precomps;
-	private int[][][] pos_gen_precomps;
-	private int[][][] pos_gen_head;
-	private Tuples[] precompTuples;
+  private int[][][] pos_head_precomps;
+  private int[][][] pos_gen_precomps;
+  private int[][][] pos_gen_head;
+  private Tuples[] precompTuples;
 
-	private TLong[][] outputTuples;
-	private int[] counters;
-	private Rule[] rules;
-	private Set<Tuple> duplicates = new HashSet<Tuple>();
-	private Tuple supportTuple = TupleFactory.newTuple();
+  private TLong[][] outputTuples;
+  private int[] counters;
+  private Rule[] rules;
+  private final Set<Tuple> duplicates = new HashSet<Tuple>();
+  private Tuple supportTuple = TupleFactory.newTuple();
 
-	@Override
-	public void startProcess(ActionContext context) throws Exception {
-		rules = ReasoningContext.getInstance().getRuleset()
-				.getAllRulesWithSchemaAndGeneric();
-		counters = new int[rules.length];
-		pos_head_precomps = new int[rules.length][][];
-		pos_gen_precomps = new int[rules.length][][];
-		pos_gen_head = new int[rules.length][][];
-		precompTuples = new Tuples[rules.length];
-		outputTuples = new TLong[rules.length][];
+  @Override
+  public void startProcess(ActionContext context) throws Exception {
+    rules = ReasoningContext.getInstance().getRuleset().getAllRulesWithSchemaAndGeneric();
+    counters = new int[rules.length];
+    pos_head_precomps = new int[rules.length][][];
+    pos_gen_precomps = new int[rules.length][][];
+    pos_gen_head = new int[rules.length][][];
+    precompTuples = new Tuples[rules.length];
+    outputTuples = new TLong[rules.length][];
 
-		for (int m = 0; m < rules.length; ++m) {
-			Rule rule = rules[m];
-			pos_head_precomps[m] = rule.getSharedVariablesHead_Precomp();
-			pos_gen_precomps[m] = rule.getSharedVariablesGen_Precomp();
-			pos_gen_head[m] = rule.getSharedVariablesGen_Head();
-			precompTuples[m] = rule.getPrecomputedTuples();
-			if (pos_gen_precomps[m].length > 1) {
-				throw new Exception("Not supported");
-			}
+    for (int m = 0; m < rules.length; ++m) {
+      Rule rule = rules[m];
+      pos_head_precomps[m] = rule.getSharedVariablesHead_Precomp();
+      pos_gen_precomps[m] = rule.getSharedVariablesGen_Precomp();
+      pos_gen_head[m] = rule.getSharedVariablesGen_Head();
+      precompTuples[m] = rule.getPrecomputedTuples();
+      if (pos_gen_precomps[m].length > 1) {
+        throw new Exception("Not supported");
+      }
 
-			// Fill the outputTriple with the constants that come from the head
-			// of the rule
-			Pattern head = rule.getHead();
-			outputTuples[m] = new TLong[3];
-			for (int i = 0; i < 3; ++i) {
-				Term t = head.getTerm(i);
-				if (t.getName() == null) {
-					outputTuples[m][i] = new TLong(t.getValue());
-				} else {
-					outputTuples[m][i] = new TLong();
-				}
-			}
-		}
-	}
+      // Fill the outputTriple with the constants that come from the head
+      // of the rule
+      Pattern head = rule.getHead();
+      outputTuples[m] = new TLong[3];
+      for (int i = 0; i < 3; ++i) {
+        Term t = head.getTerm(i);
+        if (t.getName() == null) {
+          outputTuples[m][i] = new TLong(t.getValue());
+        } else {
+          outputTuples[m][i] = new TLong();
+        }
+      }
+    }
+  }
 
-	@Override
-	public void process(Tuple tuple, ActionContext context,
-			ActionOutput actionOutput) throws Exception {
+  @Override
+  public void process(Tuple tuple, ActionContext context, ActionOutput actionOutput) throws Exception {
+    duplicates.clear();
+    TByteArray key = (TByteArray) tuple.get(0);
 
-		duplicates.clear();
-		TByteArray key = (TByteArray) tuple.get(0);
+    // Perform the join between the "value" part of the triple and the
+    // precomputed tuples. Notice that this works only if there is one
+    // element to join.
+    TBag values = (TBag) tuple.get(tuple.getNElements() - 1);
 
-		// Perform the join between the "value" part of the triple and the
-		// precomputed tuples. Notice that this works only if there is one
-		// element to join.
-		TBag values = (TBag) tuple.get(tuple.getNElements() - 1);
+    int currentRule = -1;
+    long currentJoinValue = -1;
+    for (Tuple t : values) {
+      TByte rule = (TByte) t.get(0);
+      TLong elementToJoin = (TLong) t.get(1);
+      if (rule.getValue() == currentRule && currentJoinValue == elementToJoin.getValue()) {
+        continue;
+      } else {
+        currentRule = rule.getValue();
+        currentJoinValue = elementToJoin.getValue();
+        // First copy the "key" in the output triple.
+        for (int i = 0; i < pos_gen_head[currentRule].length; ++i) {
+          outputTuples[currentRule][pos_gen_head[currentRule][i][1]].setValue(Utils.decodeLong(key.getArray(), 8 * i));
+        }
+      }
 
-		int currentRule = -1;
-		long currentJoinValue = -1;
-		for (Tuple t : values) {
-			TByte rule = (TByte) t.get(0);
-			TLong elementToJoin = (TLong) t.get(1);
-			if (rule.getValue() == currentRule
-					&& currentJoinValue == elementToJoin.getValue()) {
-				continue;
-			} else {
-				currentRule = rule.getValue();
-				currentJoinValue = elementToJoin.getValue();
-				// First copy the "key" in the output triple.
-				for (int i = 0; i < pos_gen_head[currentRule].length; ++i) {
-					outputTuples[currentRule][pos_gen_head[currentRule][i][1]]
-							.setValue(Utils.decodeLong(key.getArray(), 8 * i));
-				}
-			}
+      Collection<long[]> set = precompTuples[currentRule].get(pos_gen_precomps[currentRule][0][1], currentJoinValue);
+      if (set != null) {
+        for (long[] row : set) {
+          // Get current values
+          for (int i = 0; i < pos_head_precomps[currentRule].length; ++i) {
+            outputTuples[currentRule][pos_head_precomps[currentRule][i][0]].setValue(row[pos_head_precomps[currentRule][i][1]]);
+          }
+          supportTuple.set(outputTuples[currentRule]);
+          if (!duplicates.contains(supportTuple)) {
+            duplicates.add(supportTuple);
+            supportTuple = TupleFactory.newTuple();
+            actionOutput.output(outputTuples[currentRule]);
+            counters[currentRule]++;
+          }
+        }
+      }
+    }
+  }
 
-			Collection<long[]> set = precompTuples[currentRule].get(
-					pos_gen_precomps[currentRule][0][1], currentJoinValue);
-			if (set != null) {
-				for (long[] row : set) {
-					// Get current values
-					for (int i = 0; i < pos_head_precomps[currentRule].length; ++i) {
-						outputTuples[currentRule][pos_head_precomps[currentRule][i][0]]
-								.setValue(row[pos_head_precomps[currentRule][i][1]]);
-					}
-
-					supportTuple.set(outputTuples[currentRule]);
-					if (!duplicates.contains(supportTuple)) {
-						duplicates.add(supportTuple);
-						supportTuple = TupleFactory.newTuple();
-						actionOutput.output(outputTuples[currentRule]);
-						counters[currentRule]++;
-					}
-				}
-			}
-		}
-	}
-
-	@Override
-	public void stopProcess(ActionContext context, ActionOutput actionOutput)
-			throws Exception {
-		pos_head_precomps = null;
-		pos_gen_precomps = null;
-		pos_gen_head = null;
-		precompTuples = null;
-		outputTuples = null;
-		for (int i = 0; i < counters.length; ++i) {
-			if (counters[i] > 0) {
-				context.incrCounter("derivation-rule-" + rules[i].getId(),
-						counters[i]);
-			}
-		}
-	}
+  @Override
+  public void stopProcess(ActionContext context, ActionOutput actionOutput) throws Exception {
+    pos_head_precomps = null;
+    pos_gen_precomps = null;
+    pos_gen_head = null;
+    precompTuples = null;
+    outputTuples = null;
+    for (int i = 0; i < counters.length; ++i) {
+      if (counters[i] > 0) {
+        context.incrCounter("derivation-rule-" + rules[i].getId(), counters[i]);
+      }
+    }
+  }
 }
