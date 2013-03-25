288b6c0c54b8fa52345b922119a9e240a9d957ca
Jacopo Urbani <jacopo@cs.vu.nl>
2013-03-25 16:59:25 +0100

    bugfixes

--- src/nl/vu/cs/querypie/reasoner/actions/AddDerivationCount.java
+++ src/nl/vu/cs/querypie/reasoner/actions/AddDerivationCount.java
@@ -1,25 +1,29 @@
 package nl.vu.cs.querypie.reasoner.actions;
 
+import java.util.Arrays;
+
 import nl.vu.cs.ajira.actions.Action;
 import nl.vu.cs.ajira.actions.ActionContext;
 import nl.vu.cs.ajira.actions.ActionOutput;
 import nl.vu.cs.ajira.data.types.SimpleData;
 import nl.vu.cs.ajira.data.types.TInt;
+import nl.vu.cs.ajira.data.types.TLong;
 import nl.vu.cs.ajira.data.types.Tuple;
 import nl.vu.cs.ajira.data.types.TupleFactory;
 
 public class AddDerivationCount extends Action {
 
-	private final Tuple previousTuple = TupleFactory.newTuple();
 	private boolean first = true;
 	private int currentCount = 0;
-	private final TInt outputCount = new TInt();
 
-	private final SimpleData[] outputTuple = new SimpleData[4];
+	private final SimpleData[] outputTuple = { new TLong(), new TLong(),
+			new TLong(), new TInt() };
+	private final Tuple previousTuple = TupleFactory.newTuple(Arrays.copyOf(
+			outputTuple, 3));
+	private final TInt count = (TInt) outputTuple[3];
 
 	@Override
 	public void startProcess(ActionContext context) throws Exception {
-		outputTuple[3] = outputCount;
 		first = true;
 	}
 
@@ -31,8 +35,8 @@ public class AddDerivationCount extends Action {
 			currentCount = 1;
 			tuple.copyTo(previousTuple);
 		} else if (!previousTuple.equals(tuple)) {
-			outputCount.setValue(currentCount);
-			actionOutput.output(previousTuple);
+			count.setValue(currentCount);
+			actionOutput.output(outputTuple);
 			currentCount = 1;
 			tuple.copyTo(previousTuple);
 		} else {
@@ -44,8 +48,8 @@ public class AddDerivationCount extends Action {
 	public void stopProcess(ActionContext context, ActionOutput actionOutput)
 			throws Exception {
 		if (!first) {
-			outputCount.setValue(currentCount);
-			actionOutput.output(previousTuple);
+			count.setValue(currentCount);
+			actionOutput.output(outputTuple);
 		}
 	}
 }
