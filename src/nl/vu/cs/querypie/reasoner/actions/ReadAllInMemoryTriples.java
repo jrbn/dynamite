52c40e88742eec9389997c2e712c758f43ce5820
Jacopo Urbani <jacopo@cs.vu.nl>
2013-03-27 10:35:40 +0100

    second merge

---
+++ src/nl/vu/cs/querypie/reasoner/actions/ReadAllInMemoryTriples.java
@@ -0,0 +1,34 @@
+package nl.vu.cs.querypie.reasoner.actions;
+
+import nl.vu.cs.ajira.actions.Action;
+import nl.vu.cs.ajira.actions.ActionConf;
+import nl.vu.cs.ajira.actions.ActionContext;
+import nl.vu.cs.ajira.actions.ActionOutput;
+import nl.vu.cs.ajira.data.types.Tuple;
+import nl.vu.cs.querypie.storage.inmemory.TupleSet;
+
+public class ReadAllInMemoryTriples extends Action {
+	static final int IN_MEMORY_KEY = 0;
+	private TupleSet inMemorySet;
+
+	@Override
+	public void process(Tuple tuple, ActionContext context,
+			ActionOutput actionOutput) throws Exception {
+		for (Tuple t : inMemorySet) {
+			actionOutput.output(t);
+		}
+	}
+
+	@Override
+	public void registerActionParameters(ActionConf conf) {
+		conf.registerParameter(IN_MEMORY_KEY, "in memory key", null, true);
+	}
+
+	@Override
+	public void startProcess(ActionContext context) throws Exception {
+		String inMemoryKey = getParamString(IN_MEMORY_KEY);
+		inMemorySet = (TupleSet) context.getObjectFromCache(inMemoryKey);
+		assert (inMemorySet != null);
+	}
+
+}
